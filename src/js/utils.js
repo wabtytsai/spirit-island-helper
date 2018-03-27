1a118dd10ae64e5b4e760de9ce7ff7d5dc98a854
Edward Tsai <edward.tsai.0091+github@gmail.com>
2018-03-27 17:15:58 -0400

    Replace fear text with image
    Click once will reveal the image, click again will discard the image.

---
+++ src/js/utils.js
@@ -0,0 +1,8 @@
+export function toFilename(name, extension = "") {
+  let ret = name.toLowerCase().replace(/ /g, "_");
+  if (extension !== "") {
+    ret += "." + extension;
+  }
+  return ret;
+
+}
