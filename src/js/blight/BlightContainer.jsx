34669c9eaeb687e30dff6e3951bd7c7502d976f2
Zhaoyue Xu <zx_kevin168@hotmail.com>
2018-03-31 07:35:40 -0400

    added a way to generate 1 of 2 blight cards when the blight hits zero, and also added game over text when blight hits zero the second time

--- src/js/blight/BlightContainer.jsx
+++ src/js/blight/BlightContainer.jsx
@@ -5,18 +5,47 @@ export default class BlightContainer extends Component {
     super(props);
     this.totalBlight = this.props.players * 2;
 
-    this.state = { numBlight: this.totalBlight };
+    this.state = { 
+      numBlight: this.totalBlight,
+      blightCardName: "No Effects",
+      blightCardEffect: "",
+      endGameTrigger: 0
+    };
 
     this.handleAddBlight = this.handleAddBlight.bind(this);
     this.handleRemoveBlight = this.handleRemoveBlight.bind(this);
+    this.renderBlightCard = this.renderBlightCard.bind(this);
 
   }
+  renderBlightCard(blights) {
+    if (blights <= 1 && !this.state.endGameTrigger) {
+      var rngBlight = Math.round(Math.random());
+      if(rngBlight){
+        this.setState({ blightCardName: "Downward Spiral" });
+        this.setState({ blightCardEffect: "At the start of each invader phase, each spirit loses one presence." });
+        this.setState({ numBlight: 5 * this.props.players }); 
+      }
+      else{
+        this.setState({ blightCardName: "Memory Fades" });
+        this.setState({ blightCardEffect: "At the start of each invader phase, each spirit forgets one power or loses one presence." });
+        this.setState({ numBlight: 4 * this.props.players });
+      }
+      this.setState({ endGameTrigger: 1 });
+    }
+    if (blights <=1 && this.state.endGameTrigger) {
+      this.setState({ blightCardName: "GAME OVER" });
+      this.setState({ blightCardEffect: "" });
+    }
+  }
+
   handleAddBlight(e) {
     this.setState({ numBlight: this.state.numBlight + 1 });
   }
   handleRemoveBlight(e) {
     this.setState({ numBlight: this.state.numBlight - 1 });
+    this.renderBlightCard(this.state.numBlight);
   }
+  
   render() {
     return (
       <div className="blight-container">
@@ -29,6 +58,11 @@ export default class BlightContainer extends Component {
           <button onClick={this.handleRemoveBlight}>
             Remove Blight
           </button>
+        </div>
+        <div className="effect-name">
+          <p></p>
+          <p>{this.state.blightCardName}</p>
+          <p>{this.state.blightCardEffect}</p>
         </div>
       </div>
     );
