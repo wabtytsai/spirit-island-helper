d10223ddc6a5c73b5dc042c811f67dab44114e8d
Zhaoyue Xu <zx_kevin168@hotmail.com>
2018-04-12 01:50:49 -0400

    I have changed all of the things except making the fake card

--- src/js/blight/BlightContainer.jsx
+++ src/js/blight/BlightContainer.jsx
@@ -25,19 +25,26 @@ export default class BlightContainer extends Component {
   handleRemoveBlight(e) {
     const newBlight = this.state.numBlight - 1;
     this.setState({ numBlight: newBlight});
-    if(newBlight <= 0 && this.state.endGameTrigger){
-      this.setState({ blightCard: { "desc": "", "name": "GAME OVER", "newblights": 0 } });
+    if(newBlight <= 0 && this.state.endGameTrigger === true){
       this.setState({ numBlight: 0 });
+      this.setState({ blightCard: { "desc": "", "name": "GAME OVER", "newblights": 0 } });
     }
     else if(newBlight === 0 ){
-      this.setState({ endGameTrigger: true });
       let card = _.shuffle(blightCards)[0];
-      this.setState({ blightCard: card });
-      this.setState({ numBlight: card.newBlights*this.props.players });
+      this.setState({ numBlight: card.newBlights*this.props.players,
+                      blightCard: card,
+                      endGameTrigger: true 
+      });
     }
   }
   
   render() {
+    // Help idk y this doesnt work
+    //if(this.endGameTrigger === true){
+    //  return(
+    //    <div> GAME OVER </div>
+    //  );
+    //}
     return (
       <div className="blight-container">
         <p> Blights Counter </p>
@@ -51,7 +58,6 @@ export default class BlightContainer extends Component {
           </button>
         </div>
         <div className="effect-name">
-          <p></p>
           <p>{this.state.blightCard.name}</p>
           <p>{this.state.blightCard.desc}</p>
         </div>
